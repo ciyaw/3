57b512fb52ef51548b6bcb61400e7c30536c70de
ciyaw <140060622+ciyaw@users.noreply.github.com>
2024-09-21 16:32:29 +0800

    Add files via upload

---
+++ LuasSegitiga.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author USER
+ */
+public class LuasSegitiga {
+    public static void main(String[] args) {
+        int alas = 7;
+        int tinggi = 3 ;
+        double setengah = 0.5;
+        double luas = (double)alas*tinggi /2;  
+        System.out.println("Luas Segitiga : " + luas);
+    }
+    
+ 
+}
